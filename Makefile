42f92a5873ae39f4450c285022933f526b31b6ca
rylanperumal <rylan.perumal@gmail.com>
2019-06-17 23:01:46 +0200

    Update Serial

--- Makefile
+++ Makefile
@@ -4,13 +4,7 @@ CC=gcc
 NVCC=nvcc
 CCFLAGS=-g -Wall
 
-all: serial_rec serial_it serial_tot
-
-serial_rec: serial_n_queens.c
-		$(CC) $(CCFLAGS) serial_n_queens.c -o serial_rec
-serial_it: N_queens_iterative.c
-		$(CC) $(CCFLAGS) N_queens_iterative.c -o serial_it
-serial_tot: N_queens_serial_total.c
-		$(CC) $(CCFLAGS) N_queens_serial_total.c -o serial_tot
+all: serial
+		$(CC) $(CCFLAGS) N_queens_serial.c -o serial
 clean:
-	rm serial_rec serial_it serial_tot
+	rm serial
--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
-./serial_tot 5 |& tee -a out.txt;
-./serial_tot 6 |& tee -a out.txt;
-./serial_tot 7 |& tee -a out.txt;
-./serial_tot 8 |& tee -a out.txt;
-./serial_tot 9 |& tee -a out.txt;
-./serial_tot 10 |& tee -a out.txt;
-./serial_tot 11 |& tee -a out.txt;
-./serial_tot 12 |& tee -a out.txt;
-./serial_tot 13 |& tee -a out.txt;
-./serial_tot 14 |& tee -a out.txt;
+echo "Running serial tests... "
+echo "=============================================" |& tee -a out_serial.txt
+./serial_tot 5 |& tee -a out_serial.txt;
+./serial_tot 6 |& tee -a out_serial.txt;
+./serial_tot 7 |& tee -a out_serial.txt;
+./serial_tot 8 |& tee -a out_serial.txt;
+./serial_tot 9 |& tee -a out_serial.txt;
+./serial_tot 10 |& tee -a out_serial.txt;
+./serial_tot 11 |& tee -a out_serial.txt;
+./serial_tot 12 |& tee -a out_serial.txt;
+./serial_tot 13 |& tee -a out_serial.txt;
+./serial_tot 14 |& tee -a out_serial.txt;
+./serial_tot 15 |& tee -a out_serial.txt;
+./serial_tot 16 |& tee -a out_serial.txt;
