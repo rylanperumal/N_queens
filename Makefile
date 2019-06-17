ab478a2f47499a859f4dc039883955da97c75dbb
rylanperumal <rylan.perumal@gmail.com>
2019-06-17 23:06:43 +0200

    Serial n_queens complete

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ NVCC=nvcc
 CCFLAGS=-g -Wall
 
 all: serial
+serial: N_queens_serial.c
 		$(CC) $(CCFLAGS) N_queens_serial.c -o serial
 clean:
 	rm serial
--- run.sh
+++ run.sh
@@ -13,17 +13,3 @@ echo "=============================================" |& tee -a out_serial.txt
 ./serial_tot 14 |& tee -a out_serial.txt;
 ./serial_tot 15 |& tee -a out_serial.txt;
 ./serial_tot 16 |& tee -a out_serial.txt;
-=======
-./serial_tot 5 |& tee -a out.txt;
-./serial_tot 6 |& tee -a out.txt;
-./serial_tot 7 |& tee -a out.txt;
-./serial_tot 8 |& tee -a out.txt;
-./serial_tot 9 |& tee -a out.txt;
-./serial_tot 10 |& tee -a out.txt;
-./serial_tot 11 |& tee -a out.txt;
-./serial_tot 12 |& tee -a out.txt;
-./serial_tot 13 |& tee -a out.txt;
-./serial_tot 14 |& tee -a out.txt;
-./serial_tot 15 |& tee -a out.txt;
-./serial_tot 16 |& tee -a out.txt;
->>>>>>> 66fa7537cff817c4add96de59bd2cc97843cb079
